5f5899b96d5f7257caf034a76bc679b518e352c4
neutron1331 - school <[valid-email]>
2025-09-24 13:18:46 +0200

    added some exponents - slowly starting using javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-document.getElementById("demo").innerHTML = "Demo text";
+
+
+const num_of_letters = 5;
+let x=5 ;
+
+let y = x ** num_of_letters;
+
+document.getElementById("demo").innerHTML = "num is " + y;
